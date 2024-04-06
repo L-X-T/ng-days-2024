dc29881cfdc2a1b066223aeb1b6d93dfa7ae96d8
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-06 10:44:38 +0200

    Prepare p3rf workshop

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BlinkService } from '../shared/blink.service';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
@@ -7,5 +8,13 @@ import { NgOptimizedImage } from '@angular/common';
   standalone: true,
   imports: [RouterModule, NgOptimizedImage],
   templateUrl: 'sidebar.component.html',
+  styleUrl: 'sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  private readonly blinkService = inject(BlinkService);
+  private readonly elementRef = inject(ElementRef);
+
+  blink(): void {
+    this.blinkService.blinkElementsFirstChild(this.elementRef);
+  }
+}
