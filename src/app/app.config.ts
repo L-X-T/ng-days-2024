8a030c927f47022378cca4f4e3165b2f16b51a98
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-24 10:38:37 +0200

    Add ngx-quicklink

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 import { provideImageKitLoader } from '@angular/common';
+import { quicklinkProviders, QuicklinkStrategy } from 'ngx-quicklink';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,8 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       appRoutes,
+      withPreloading(QuicklinkStrategy),
       // withDebugTracing(),
       // withEnabledBlockingInitialNavigation()
     ),
+    quicklinkProviders,
   ],
 };
