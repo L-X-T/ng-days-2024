2f2747df5c15c7aa1b2b52bf4ba5ffd671f0ff6c
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-24 10:37:48 +0200

    Fix global.ts

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,5 +1,7 @@
 import { FlightService } from '../flights/flight.service';
 
 // intentional circular dependency
-export const apiUrl = FlightService.USE_HTTPS ? 'https://demo.angulararchitects.io/api/' : 'http://www.angular.at/api/';
+export const apiUrl = FlightService?.USE_HTTPS
+  ? 'https://demo.angulararchitects.io/api/'
+  : 'http://www.angular.at/api/';
 export const pattern = '[a-zA-ZäöüÄÖÜß ]*';
