2617d5be080c18d733303ed3f98c914ae3d0904b
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-10-24 10:23:46 +0200

    NgOptimizedImage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
+import { provideImageKitLoader } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
+    provideImageKitLoader('https://ik.imagekit.io/LXT'),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       appRoutes,
